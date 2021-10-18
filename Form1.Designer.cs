a81eb829c14c3dbf412dc6e8e9781e9dd31f0fcf
Alex Gabriel Oyola <gabriel_oyola@outlook.com>
2021-10-18 11:33:41 -0300

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿
+namespace MiPrimerArreglo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btDias = new System.Windows.Forms.Button();
+            this.lblDias = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btDias
+            // 
+            this.btDias.Location = new System.Drawing.Point(12, 12);
+            this.btDias.Name = "btDias";
+            this.btDias.Size = new System.Drawing.Size(75, 23);
+            this.btDias.TabIndex = 0;
+            this.btDias.Text = "DIAS";
+            this.btDias.UseVisualStyleBackColor = true;
+            this.btDias.Click += new System.EventHandler(this.btDias_Click);
+            // 
+            // lblDias
+            // 
+            this.lblDias.AutoSize = true;
+            this.lblDias.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDias.Location = new System.Drawing.Point(32, 38);
+            this.lblDias.Name = "lblDias";
+            this.lblDias.Size = new System.Drawing.Size(0, 16);
+            this.lblDias.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblDias);
+            this.Controls.Add(this.btDias);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btDias;
+        private System.Windows.Forms.Label lblDias;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MiPrimerArreglo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btDias_Click(object sender, EventArgs e)
+        {
+            string[] Dias = new string[7];
+
+            Dias[0] = "Lunes";
+            Dias[1] = "Martes";
+            Dias[2] = "Miercoles";
+            Dias[3] = "Jueves";
+            Dias[4] = "Viernes";
+            Dias[5] = "Sabado";
+            Dias[6] = "Domingo";
+
+            foreach (string item in Dias)
+            {
+                lblDias.Text = lblDias.Text + item + "\r\n";
+            }
+
+
+        }
+    }
+}
